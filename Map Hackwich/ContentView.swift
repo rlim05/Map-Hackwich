b401dc5c3dc60cf8eded6a5f1a44019cb6dcd5a9
Ryan Lim <rlim@bsd220.org>
2022-02-22 10:48:27 -0600

    Added a Location to the Map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -19,11 +19,45 @@ struct ContentView: View {
     )
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
+    @State private var places = [Place(name: "Barrington High School",
+                                       coordinate: CLLocationCoordinate2D(
+                                        latitude: 42.15704, longitude: -88.14812))]
     var body: some View {
         Map(coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode)
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
+        }
+    }
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct Marker: View {
+    var name: String
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer(minLength: 15)
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .rotationEffect(.degrees(45))
+            }
+            Capsule()
+                .fill(Color.red)
+                .frame(width: 200, height: 30, alignment: .center)
+            Text(name)
+        }
     }
 }
 
@@ -32,3 +66,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
