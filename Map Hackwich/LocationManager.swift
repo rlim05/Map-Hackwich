acb09e7389ed1135ed88e2cc018e4c4cbc5f8c8a
Ryan Lim <rlim@bsd220.org>
2022-02-22 10:37:19 -0600

    Added User Location

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Ryan Lim on 2/22/22.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
